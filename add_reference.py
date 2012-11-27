66028d1b1ef47788121651797edb227c8807b753
Zhu Tao <zhutao.iscas@gmail.com>
2012-11-27 17:21:08 +0800

    add a new post about work

--- add_reference.py
+++ add_reference.py
@@ -18,37 +18,44 @@ parser.add_option("-p", "--path", dest="path", help="Path to the markdown file t
 (options, args) = parser.parse_args()
 
 def get_marks(cont):
-	import re
-	rx = re.compile(r"(\[[^]]+\])(\[[^]]+\])")
-	matches = rx.findall(cont)
-	ret = {}
-	for match in matches:
-		ret.update({match[0] : match[1]})
-	return ret
+    import re
+    rx = re.compile(r"(\[[^]]+\])(\[[^]]+\])")
+    matches = rx.findall(cont)
+    ret = {}
+    for match in matches:
+        ret.update({match[0] : match[1]})
+    return ret
 
+def append_link(cont):
+    import re
+    rx = re.compile(r"(\[[^]]+\])")
+    out = rx.sub(r"\1\1", cont)
+    return out
+    
 
 if options.path:
-	print "Add Reference Starts ..."
-	fpath = options.path
-	fh = open(fpath, "r")
-	cont = fh.read()
-	fh.close()
-	matches = get_marks(cont)
-	references = ""
-	count = 1
-	for key, value in matches.iteritems():
-		references += "%d. %s%s\n" % (count, key, value)
-		count += 1
-	
-	links = ""
-	values = matches.values()
-	for value in values:
-		links += "%s:\n" % value
-	after = skeleton % (cont, references, links)
-	fh = open(fpath, "w")
-	fh.write(after)
-	fh.close()
-	print "Add Reference Done."
+    print "Add Reference Starts ..."
+    fpath = options.path
+    fh = open(fpath, "r")
+    cont = fh.read()
+    fh.close()
+    cont = append_link(cont)
+    matches = get_marks(cont)
+    references = ""
+    count = 1
+    for key, value in matches.iteritems():
+        references += "%d. %s%s\n" % (count, key, value)
+        count += 1
+    
+    links = ""
+    values = matches.values()
+    for value in values:
+        links += "%s:\n" % value
+    after = skeleton % (cont, references, links)
+    fh = open(fpath, "w")
+    fh.write(after)
+    fh.close()
+    print "Add Reference Done."
 
 else:
-	parser.print_help()
+    parser.print_help()
